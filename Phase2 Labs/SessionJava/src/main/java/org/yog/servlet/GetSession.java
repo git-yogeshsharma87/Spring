f19f01d25f91ad009a979fe298423cc212280eaa
Yogesh <yogeshrajasharma87@gmail.com>
2021-09-09 02:31:10 +0530

    Session servlet example

---
+++ Phase2 Labs/SessionJava/src/main/java/org/yog/servlet/GetSession.java
@@ -0,0 +1,46 @@
+package org.yog.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class GetSession
+ */
+@WebServlet("/GetSession")
+public class GetSession extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GetSession() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		HttpSession session = request.getSession(false);
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("<html><body>");
+		
+		out.println(session.getAttribute("uid"));
+		
+		out.println("</body></html>");
+	}
+
+	
+
+}
